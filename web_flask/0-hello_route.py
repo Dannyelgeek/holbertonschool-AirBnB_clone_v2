938542369aaabf3734370a3334dcdc877c675261
Dannyelgeek <daot1993@gmail.com>
2023-04-04 17:59:39 -0500

    AirBnB clone: Web framework task 0.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,11 +3,13 @@
 from flask import Flask
 
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
