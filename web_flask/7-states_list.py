c5eb6ef3e4014343c49b1aaa852ae95d02cbc64c
Dannyelgeek <daot1993@gmail.com>
2023-04-06 11:05:42 -0500

    AirBnB clone: Web framework task 7.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' starts a Flask web application'''
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def index():
+    stg = storage.all(State).values()
+    data = {
+        'show_stg': stg
+    }
+    return render_template('7-states_list.html', data=data)
+
+@app.teardown_appcontext
+def stg_close():
+    '''remove the current SQLAlchemy Session'''
+    storage.close()
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
