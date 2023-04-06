4d3f3872d0bc510ae09aeef5fecc9a6207c1518c
Dannyelgeek <daot1993@gmail.com>
2023-04-06 11:08:47 -0500

    AirBnB clone: Web framework task 7.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,10 +11,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def index():
     stg = storage.all(State).values()
-    data = {
-        'show_stg': stg
-    }
-    return render_template('7-states_list.html', data=data)
+    return render_template('7-states_list.html', stg=stg)
 
 @app.teardown_appcontext
 def stg_close():
